f89094c6f1871276960d67cbf0f872606f1f468f
AndreaLuigiPala <andrealuigipala@gmail.com>
2026-04-08 12:52:27 +0200

    Rename final_model directory to trained_model

--- scripts/evaluate_and_save.py
+++ scripts/evaluate_and_save.py
@@ -9,7 +9,7 @@ from huggingface_hub import snapshot_download
 
 HF_USERNAME  = "andrealuigipala"
 HF_REPO      = "trained-model"
-MODEL_DIR    = "./data/results/final_model"
+MODEL_DIR    = "./data/results/trained_model"
 DATA_URL     = "https://raw.githubusercontent.com/andrealuigipala-del/MLOps_sentiment_project/refs/heads/main/data/src/Twitter_Data.csv"
 SAMPLE_SIZE  = 150
 
