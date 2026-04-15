261636504c6f7563bed8223fbf2667b8f3e9cd1e
AndreaLuigiPala <andrealuigipala@gmail.com>
2026-04-15 11:10:39 +0200

    Update monitor.py

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -12,7 +12,7 @@ LOG_PATH    = "./data/monitoring/metrics_log.json"
 MODEL_DIR   = "./data/results/trained_model"
 DATA_URL    = "https://raw.githubusercontent.com/andrealuigipala-del/MLOps_sentiment_project/refs/heads/main/data/src/Twitter_Data.csv"
 F1_THRESHOLD = 0.7
-SAMPLE_SIZE  = 200
+SAMPLE_SIZE  = 150
 
 
 def load_log():
